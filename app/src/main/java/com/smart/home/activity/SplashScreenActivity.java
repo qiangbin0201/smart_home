a9fa72185fc33091dc3264bb957dcac4bba26933
lenovo <394502544@qq.com>
2017-04-08 21:33:46 +0800

    test sourcetree

--- app/src/main/java/com/smart/home/activity/SplashScreenActivity.java
+++ app/src/main/java/com/smart/home/activity/SplashScreenActivity.java
@@ -1,17 +1,15 @@
 package com.smart.home.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * Created by lenovo on 2017/4/4.
+ * Created by lenovo on 2017/4/8.
  */
 
-public class HomeActivity extends BaseActivity {
-
+public class SplashScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-
 }
